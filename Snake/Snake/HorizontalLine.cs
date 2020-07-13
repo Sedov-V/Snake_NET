9ab922b3014b08c27337d80479e7b9c58310a0df
Василий Седов <v.sedov1999@yandex.ru>
2020-07-13 11:22:29 +0300

    Lesson 8

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -6,22 +6,14 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> pointList;
-
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pointList = new List<Point>();
 
             for (int x = xLeft; x <= xRight; x++)
                 pointList.Add(new Point(x, y, sym));
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in pointList)
-                p.Draw();
         }
     }
 }
--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,22 +6,14 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pointList;
-
         public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pointList = new List<Point>();
 
             for (int y = yUp; y <= yDown; y++)
                 pointList.Add(new Point(x, y, sym));
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in pointList)
-                p.Draw();
         }
     }
 }
