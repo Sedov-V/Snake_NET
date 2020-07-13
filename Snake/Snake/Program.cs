b255d430847fb0a9baa16862686eabfbc9f46cb5
Василий Седов <v.sedov1999@yandex.ru>
2020-07-13 11:03:27 +0300

    Lesson 6

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,11 +10,30 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            /*
             Point p1 = new Point(1, 4, '#');
             p1.Draw();
 
             Point p2 = new Point(6, 9, '*');
             p2.Draw();
+            */
+
+            List<int> numList = new List<int>();
+
+            numList.Add(3);
+            numList.Add(7);
+            numList.Add(1);
+            numList.Add(2);
+            numList.Add(9);
+
+            foreach (int num in numList)
+                Console.WriteLine(num);
+
+            Console.WriteLine();
+
+            numList.Sort();
+            foreach (int num in numList)
+                Console.WriteLine(num);
 
             Console.ReadLine();
         }
